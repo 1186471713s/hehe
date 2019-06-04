a76e1a970ee5d0020e4f501d412966e6311a2430
zhang <zahng@163.com>
2019-06-04 19:34:08 +0800

    99乘法表

--- src/main/java/com/woniuxy/dome/Dome.java
+++ src/main/java/com/woniuxy/dome/Dome.java
@@ -3,7 +3,12 @@ package com.woniuxy.dome;
 public class Dome {
 
 	public static void main(String[] args) {
-		System.out.println("12343");
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			System.out.println();
+		}
 	}
 
 }
